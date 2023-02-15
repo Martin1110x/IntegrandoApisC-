417624d4a5a415b9f70fc1a85b286e19e736ce1a
Martín Regazzoli <72031552+Martin1110x@users.noreply.github.com>
2023-02-15 12:25:46 -0300

    arreglo_detalles

--- SistemaGestionWebApi/Modelos/Usuario.cs
+++ SistemaGestionWebApi/Modelos/Usuario.cs
@@ -73,4 +73,4 @@ namespace SistemaGestionWebApi.Modelos
 }
 
 
-}
+
--- SistemaGestionWebApi/Program.cs
+++ SistemaGestionWebApi/Program.cs
@@ -1,3 +1,4 @@
+from Modelos import *
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -23,3 +24,5 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+Producto producto1 = Producto()
